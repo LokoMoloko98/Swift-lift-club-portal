ec717cade869cdd6a45d5dbd851b51210683d640
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-01-21 19:43:09 +0200

    Change API Gateway route method from GET to POST for fare calculation

--- networking/apigateway_routes.tf
+++ networking/apigateway_routes.tf
@@ -1,5 +1,5 @@
 resource "aws_apigatewayv2_route" "fare-calculation" {
   api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
-  route_key = "GET /fare"
+  route_key = "POST /fare"
   target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-apigateway-lambda-integration.id}"
 }
