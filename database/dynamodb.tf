27067d9c977e030f2f6f6594241d23f770975c46
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-01-21 21:56:46 +0200

    Refactor DynamoDB users table configuration: comment out existing resource definition

--- database/dynamodb.tf
+++ database/dynamodb.tf
@@ -1,41 +1,43 @@
-resource "aws_dynamodb_table" "users-dynamodb-table" {
-  name           = "${var.project_name}-users"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "user_id"
+# resource "aws_dynamodb_table" "users-dynamodb-table" {
+#   name           = "${var.project_name}-users"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 20
+#   write_capacity = 20
+#   hash_key       = "user_id"
 
-  attribute {
-    name = "user_id"
-    type = "S"
-  }
+#   attribute {
+#     name = "user_id"
+#     type = "S"
+#   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = true
-  }
+#   ttl {
+#     attribute_name = "TimeToExist"
+#     enabled        = true
+#   }
 
-  point_in_time_recovery {
-    enabled = true
-  }
+#   point_in_time_recovery {
+#     enabled = true
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      write_capacity,
-      read_capacity
-    ]
-  }
+#   lifecycle {
+#     ignore_changes = [
+#       write_capacity,
+#       read_capacity
+#     ]
+#   }
 
-  tags = {
-    Name        = "${var.project_name}-users-table"
-    Environment = "production"
-    Costs       = "${var.project_name}"
-  }
-}
+#   tags = {
+#     Name        = "${var.project_name}-users-table"
+#     Environment = "production"
+#     Costs       = "${var.project_name}"
+#   }
+# }
 
 resource "aws_dynamodb_table" "trips-dynamodb-table" {
   name         = "${var.project_name}-trips"
   billing_mode = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
 
   attribute {
     name = "passenger_id"
