3bbad31ac74a375348d93c1351e8ea2d41b9ca7a
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-01-25 22:00:06 +0200

    Add new API Gateway routes for trip management and update fare route method

--- networking/apigateway_routes.tf
+++ networking/apigateway_routes.tf
@@ -1,5 +1,23 @@
 resource "aws_apigatewayv2_route" "fare-calculation" {
   api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
-  route_key = "POST /fare"
+  route_key = "GET /fare"
+  target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-apigateway-lambda-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "create-trip-record" {
+  api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
+  route_key = "POST /trips/add"
+  target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-apigateway-lambda-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "update-trip-record" {
+  api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
+  route_key = "PUT /trips/update"
+  target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-apigateway-lambda-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "get-weekly-trips" {
+  api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
+  route_key = "GET /trips/weekly"
   target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-apigateway-lambda-integration.id}"
 }
