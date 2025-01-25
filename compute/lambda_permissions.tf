961392732399ee00e8f123e12725f1932757c974
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-01-25 17:37:50 +0200

    🐛 Rename Lambda permission resources for fare calculation and trips table operations

--- compute/lambda_permissions.tf
+++ compute/lambda_permissions.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_permission" "lambda_apigateway_permission" {
+resource "aws_lambda_permission" "lambda_apigateway_permission_fare_calculation" {
   statement_id  = "apigateway-invoke-permissions"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.fare_calculation.function_name
@@ -6,7 +6,7 @@ resource "aws_lambda_permission" "lambda_apigateway_permission" {
   source_arn = "${var.apigateway_arn}/*/*/*"
 }
 
-resource "aws_lambda_permission" "lambda_apigateway_permission" {
+resource "aws_lambda_permission" "lambda_apigateway_permission_trips_table_ops" {
   statement_id  = "apigateway-invoke-permissions"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.trips_table_ops.function_name
