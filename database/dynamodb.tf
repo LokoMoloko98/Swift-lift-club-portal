1831044ce0b2d59a2e67b4cf3dd8501408c35c35
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-01-25 18:23:04 +0200

    Add new Lambda function for trips table operations and permissions; update fare calculation function version

--- database/dynamodb.tf
+++ database/dynamodb.tf
@@ -1,50 +1,63 @@
-resource "aws_dynamodb_table" "users-dynamodb-table" {
-  name           = "${var.project_name}-users"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "user_id"
+# resource "aws_dynamodb_table" "users-dynamodb-table" {
+#   name           = "${var.project_name}-users"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 20
+#   write_capacity = 20
+#   hash_key       = "user_id"
 
-  attribute {
-    name = "user_id"
-    type = "S"
-  }
+#   attribute {
+#     name = "user_id"
+#     type = "S"
+#   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = true
-  }
+#   ttl {
+#     attribute_name = "TimeToExist"
+#     enabled        = true
+#   }
 
-  point_in_time_recovery {
-    enabled = true
-  }
+#   point_in_time_recovery {
+#     enabled = true
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      write_capacity,
-      read_capacity
-    ]
-  }
+#   lifecycle {
+#     ignore_changes = [
+#       write_capacity,
+#       read_capacity
+#     ]
+#   }
 
-  tags = {
-    Name        = "${var.project_name}-users-table"
-    Environment = "production"
-    Costs       = "${var.project_name}"
-  }
-}
+#   tags = {
+#     Name        = "${var.project_name}-users-table"
+#     Environment = "production"
+#     Costs       = "${var.project_name}"
+#   }
+# }
 
 resource "aws_dynamodb_table" "trips-dynamodb-table" {
-  name           = "${var.project_name}-trips"
-  billing_mode   = "PROVISIONED"
+  name         = "${var.project_name}-trips"
+  billing_mode = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
-  hash_key       = "trip_id"
 
   attribute {
-    name = "trip_id"
+    name = "passenger_id"
+    type = "S" # S for String
+  }
+
+  attribute {
+    name = "trip_date_time"
     type = "S"
   }
 
+ # Table Keys
+  hash_key  = "passenger_id" # Partition Key
+  range_key = "trip_date_time"    # Sort Key
+
+  # Enable stream if needed for real-time updates
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE" # Capture new image of the item after modification
+
+  # Global secondary indexes can be added here if needed
   ttl {
     attribute_name = "TimeToExist"
     enabled        = true
