8bffca112e20904750fd8182eef0a42499e96ac9
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-02-02 22:24:08 +0200

    🔧 Add API Gateway permissions for user profile Lambda function

--- compute/lambda_permissions.tf
+++ compute/lambda_permissions.tf
@@ -13,3 +13,11 @@ resource "aws_lambda_permission" "lambda_apigateway_permission_trips_table_ops" 
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${var.apigateway_arn}/*/*/*"
 }
+
+resource "aws_lambda_permission" "lambda_apigateway_permission_user_profile" {
+  statement_id  = "apigateway-invoke-permissions"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_user_profile.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.apigateway_arn}/*/*/*"
+}
