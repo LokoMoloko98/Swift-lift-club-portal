133690765fdd3d4a0568b770f0913ebd9d9c0a5c
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-02-02 18:51:33 +0200

    🔧 Add CORS configuration and JWT authorization to API Gateway routes

--- networking/apigateway_routes.tf
+++ networking/apigateway_routes.tf
@@ -1,23 +1,31 @@
 resource "aws_apigatewayv2_route" "fare-calculation" {
-  api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
-  route_key = "GET /fare"
-  target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-fare-calculation-apigateway-lambda-integration.id}"
+  api_id             = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
+  route_key          = "GET /fare"
+  target             = "integrations/${aws_apigatewayv2_integration.swift-lift-club-fare-calculation-apigateway-lambda-integration.id}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.swift_lift_club_authorizer.id
 }
 
 resource "aws_apigatewayv2_route" "create-trip-record" {
-  api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
-  route_key = "POST /trips/add"
-  target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-trips-ops-apigateway-lambda-integration.id}"
+  api_id             = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
+  route_key          = "POST /trips/add"
+  target             = "integrations/${aws_apigatewayv2_integration.swift-lift-club-trips-ops-apigateway-lambda-integration.id}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.swift_lift_club_authorizer.id
 }
 
 resource "aws_apigatewayv2_route" "update-trip-record" {
-  api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
-  route_key = "PUT /trips/update"
-  target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-trips-ops-apigateway-lambda-integration.id}"
+  api_id             = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
+  route_key          = "PUT /trips/update"
+  target             = "integrations/${aws_apigatewayv2_integration.swift-lift-club-trips-ops-apigateway-lambda-integration.id}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.swift_lift_club_authorizer.id
 }
 
 resource "aws_apigatewayv2_route" "get-weekly-trips" {
-  api_id    = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
-  route_key = "GET /trips/weekly"
-  target    = "integrations/${aws_apigatewayv2_integration.swift-lift-club-trips-ops-apigateway-lambda-integration.id}"
+  api_id             = aws_apigatewayv2_api.swift-lift-club-api-gateway.id
+  route_key          = "GET /trips/weekly"
+  target             = "integrations/${aws_apigatewayv2_integration.swift-lift-club-trips-ops-apigateway-lambda-integration.id}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.swift_lift_club_authorizer.id
 }
