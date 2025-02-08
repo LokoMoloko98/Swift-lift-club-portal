0f4f1d20ab04720779e0f7c56536e659573a7ad4
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2025-02-08 18:47:50 +0200

    🔧 Comment out Cognito authenticated role output for future reference

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -2,6 +2,6 @@ output "lambda-dynamodb-role-arn" {
   value = aws_iam_role.swift-lift-club-lambda-role.arn
 }
 
-output "cognito-authenticated-role-arn" {
-  value = aws_iam_role.cognito_authenticated_role.arn
-}
+# output "cognito-authenticated-role-arn" {
+#   value = aws_iam_role.cognito_authenticated_role.arn
+# }
